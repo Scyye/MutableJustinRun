db2e6dea6ee7eac854e7386c3924478e333fff54
Scyye <97131358+Scyye@users.noreply.github.com>
2023-04-06 14:04:18 -0400

    Add files via upload

---
+++ Main.cs
@@ -0,0 +1,67 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace MuteableJustinRun
+{
+    [BepInPlugin(ModId, "Mutable Justin Run", "1.0.0")]
+    [BepInProcess("Justin run 2.exe")]
+    public class Main : BaseUnityPlugin
+    {
+        private const string ModId = "scyye.mutablejustinrun";
+
+        private Player player;
+        private GameObject musicPlayer;
+
+        private void Awake()
+        {
+            SceneManager.activeSceneChanged += ChangedActiveScene;
+            ToggleSound();
+        }
+
+        private void OnDestroy()
+        {
+            SceneManager.activeSceneChanged -= ChangedActiveScene;
+        }
+
+        private void ChangedActiveScene(Scene current, Scene next)
+        {
+            preInit();
+            System.Threading.Thread.Sleep(2000);
+            ToggleSound();
+        }
+
+        private void preInit()
+        {
+            player = GameObject.FindGameObjectWithTag("Player")?.GetComponent<Player>();
+            musicPlayer = GameObject.Find("music player");
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.M))
+            {
+                ToggleSound();
+            }
+        }
+
+        private void ToggleSound()
+        {
+            if (player == null)
+            {
+                player = GameObject.FindGameObjectWithTag("Player")?.GetComponent<Player>();
+            }
+            if (musicPlayer == null)
+            {
+                musicPlayer = GameObject.Find("music player");
+            }
+            if (player != null && musicPlayer != null)
+            {
+                musicPlayer.SetActive(!musicPlayer.activeSelf);
+                player.playerSoundSource.mute = !player.playerSoundSource.mute;
+            }
+        }
+    }
+}
